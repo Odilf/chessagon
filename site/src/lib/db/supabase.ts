29b5d4079878c22913c92dd0cb46a873d7f92cc8
Odilf <odysseas.maheras@gmail.com>
2023-09-26 19:48:08 +0200

    added supabase local development scaffolding

--- site/src/lib/db/supabase.ts
+++ site/src/lib/db/supabase.ts
@@ -1,7 +1,13 @@
 import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { PUBLIC_ANON_KEY, PUBLIC_SUPABASE_URL, PUBLIC_ANON_KEY_DEV, PUBLIC_SUPABASE_URL_DEV } from "$env/static/public";
+import { dev } from '$app/environment';
 
-export const supabase = createClient(
+export const supabase = dev 
+? createClient(
+	PUBLIC_SUPABASE_URL_DEV,
+	PUBLIC_ANON_KEY_DEV,
+) 
+: createClient(
 	PUBLIC_SUPABASE_URL,
-	PUBLIC_ANON_KEY
+	PUBLIC_ANON_KEY,
 )
