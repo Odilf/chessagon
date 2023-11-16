cd9d8fbbd181cc035d561b10d24f711e9f0b5c32
Odilf <odysseas.maheras@gmail.com>
2023-11-16 20:39:09 +0100

    added move number

--- site/src/lib/board/Clock.svelte
+++ site/src/lib/board/Clock.svelte
@@ -48,35 +48,45 @@
 </script>
 
 <div class="card variant-ghost-surface p-4 flex lg:flex-col lg:px-8">
-  <div class="{turn === playerColor && "opacity-50"} transition flex-1 font-bold text-2xl lg:text-center -z-10">
+  <div
+    class="{turn === playerColor &&
+      'opacity-50'} transition flex-1 font-bold text-2xl lg:text-center -z-10"
+  >
     Opponent
     <div class="font-mono font-thin text-3xl">
       {formatTime(time.opponent)}
     </div>
   </div>
 
-  <div
-    class="{turn === playerColor
-      ? 'lg:rotate-90'
-      : 'lg:-rotate-90 rotate-180'} w-fit transition-transform py-4 mx-4 lg:mx-auto text-surface-400 -z-10"
-  >
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="4"
-      stroke="currentColor"
-      class="w-8 h-8"
+  <div class="flex flex-col lg:flex-row mx-auto items-center">
+    <div
+      class="{turn === playerColor
+        ? 'lg:rotate-90'
+        : 'lg:-rotate-90 rotate-180'} w-fit h-fit transition-transform py-4 mx-4 text-surface-400 -z-10"
     >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
-      />
-    </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="4"
+        stroke="currentColor"
+        class="w-8 h-8"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+        />
+      </svg>
+    </div>
+
+    <div class="text-xl text-surface-400">{Math.ceil(moves.length / 2)}</div>
   </div>
 
-  <div class="{turn !== playerColor && "opacity-50"} transition flex-1 font-bold text-2xl text-right lg:text-center -z-10">
+  <div
+    class="{turn !== playerColor &&
+      'opacity-50'} transition flex-1 font-bold text-2xl text-right lg:text-center -z-10"
+  >
     You
     <div class="font-mono font-thin text-3xl">
       {formatTime(time.player)}
