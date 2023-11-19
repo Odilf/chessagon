f3d7804d3c7c58ab6e7712d87fb0e96fb89a89e3
Odilf <odysseas.maheras@gmail.com>
2023-11-19 14:57:05 +0100

    added dev vs prod database urls

--- site/drizzle.config.ts
+++ site/drizzle.config.ts
@@ -1,11 +1,15 @@
 import type { Config } from "drizzle-kit";
 import "dotenv/config";
 
+const url = process.argv.includes("--prod")
+  ? process.env.DATABASE_URL_PROD
+  : process.env.DATABASE_URL_DEV;
+
 export default {
   schema: "./src/lib/db/schema.ts",
   out: "./drizzle",
   driver: "libsql",
   dbCredentials: {
-    url: process.env.DATABASE_URL_DEV,
+    url,
   },
 } satisfies Config;
--- site/src/lib/db/index.ts
+++ site/src/lib/db/index.ts
@@ -1,12 +1,12 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
-import { DATABASE_URL_DEV } from "$env/static/private";
+import { DATABASE_URL_DEV, DATABASE_URL_PROD } from "$env/static/private";
 import { dev } from "$app/environment";
 import todo from "ts-todo";
 import * as schema from "./schema";
 
 export const client = createClient({
-  url: dev ? DATABASE_URL_DEV : todo(),
+  url: dev ? DATABASE_URL_DEV : DATABASE_URL_PROD,
 });
 
 export const db = drizzle(client, { schema });
