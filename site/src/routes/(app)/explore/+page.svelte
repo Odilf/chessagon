3b4913a30d782c4238891adb116493d67e927ab1
Odilf <odysseas.maheras@gmail.com>
2023-11-19 13:45:07 +0100

    added WIP `explore` page

---
+++ site/src/routes/(app)/explore/+page.svelte
@@ -0,0 +1,9 @@
+<main class="grid place-content-center h-full">
+	<span class="text-center font-bold">
+		Work in progress...
+	</span>
+
+	<a href="/" class="anchor">
+		Go back to main page
+	</a>
+</main>
