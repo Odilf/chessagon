feab127329c8131a6dd971ec1f067a3d5bd153e3
Odilf <odysseas.maheras@gmail.com>
2023-11-20 14:19:44 +0100

    changed url thingy, jic

--- site/src/lib/db/index.ts
+++ site/src/lib/db/index.ts
@@ -2,10 +2,10 @@ import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 import { DATABASE_URL_DEV, DATABASE_URL_PROD, TURSO_AUTH_TOKEN } from "$env/static/private";
 import * as schema from "./schema";
+import { dev } from "$app/environment";
 
 export const client = createClient({
-  url: DATABASE_URL_DEV,
-  syncUrl: DATABASE_URL_PROD,
+  url: dev ? DATABASE_URL_DEV : DATABASE_URL_PROD,
   authToken: TURSO_AUTH_TOKEN,
 });
 
